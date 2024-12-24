b40d613a123b5b3167652a784c79ba9c3c6efef4
Butsushi <36818362+Laurenceshao@users.noreply.github.com>
2024-12-23 23:52:12 -0600

    added search bar

--- ai-companion/app/layout.tsx
+++ ai-companion/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs"
 import { ThemeProvider } from "@/components/theme-provider";
+import { cn } from "@/lib/utils";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,7 @@ export default function RootLayout({
   return ( 
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <body className={cn("bg-secondary", `${geistSans.variable} ${geistMono.variable} antialiased`)}>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             {children}
           </ThemeProvider>
